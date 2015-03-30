92720447c716711ad87f22e8cc17ed47a23440c6
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-03-30 12:23:26 +0100

    Added cat to homepage

--- hello.rb
+++ hello.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "hello world welcome to paradise!
+  <div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+end
+
+get '/secret' do
+  'This is a secret page'
 end
